fde5614cacaa65834518ccc92cc5233363e49bc5
Jacob <25849954+JmSMI@users.noreply.github.com>
2020-06-30 11:10:34 -0700

    Update and rename get-autoReplyEnabledUsers to get-autoReplyEnabledUsers.ps1

--- get-autoReplyEnabledUsers.ps1
+++ get-autoReplyEnabledUsers.ps1
@@ -3,6 +3,8 @@ Get a list of all users who have an auto reply on or scheduled to be on
 Author: James
 Date: June 30 2020
 Version 1.0
+
+Requirements: Connect to Exchange Online Module for Office365
 #>
 get-user | Where-Object {($_.company -eq "David Suzuki Foundation")} | 
 Get-MailboxAutoReplyConfiguration | 
