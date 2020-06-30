d21b570f21726872586553ae4070060898682815
Jacob <25849954+JmSMI@users.noreply.github.com>
2020-06-30 11:12:13 -0700

    Create Add-NewPcToDomain.ps1

---
+++ Add-NewPcToDomain.ps1
@@ -0,0 +1,27 @@
+<# This is where I'll learn to use the workflow function to continue scripts after a reboot.
+Figure out the name you need before running this. It will rename the local pc to a new name, and then join that computer to the dsf domain
+Date: June 30 2020        
+#>
+
+# Creates a new name based on your input
+# First, specify the name of your workflow. You will call this upon reboot in powershell (as admin!)
+
+workflow Rename-And-Reboot {
+    
+    param ([string]$Name)
+    
+    Read-Host -Prompt "Enter new computer name" -Outvariable computername
+
+    Rename-Computer -NewName "$computername" -Confirm
+
+    Restart-Computer -Wait
+}
+
+
+# After a reboot, the computer is joined to the kits.davidsuzuki.org domain
+
+Read-Host -Prompt "Enter admin username" -Outvariable username | Out-Null
+
+$cred = Get-Credential -credential "$username"
+
+Add-Computer -domainName kits.davidsuzuki.org -Credential $cred -PassThru -Verbose -Restart
