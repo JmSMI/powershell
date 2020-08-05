ee43e8f987525c0dd8ba6d04c2f08ec7c4e687d2
Jacob <25849954+JmSMI@users.noreply.github.com>
2020-08-05 13:00:37 -0700

    Update Import-ExchangeModuleMFA.ps1

--- Import-ExchangeModuleMFA.ps1
+++ Import-ExchangeModuleMFA.ps1
@@ -1,4 +1,6 @@
-#You need to have already installed the Module before this script will work
+#Author: Microsoft
+#Use this block if you need to make scripts that interact with Exchange Online
+
 #This script will let you interact with Exchange online in an editor
 $module = ((Get-ChildItem -Path $($env:LOCALAPPDATA + "\Apps\2.0\") `
     -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse).FullName | 
