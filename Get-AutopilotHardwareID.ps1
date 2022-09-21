5d27b0d4936879dabd59a7a53e626647dc54464e
James <25849954+JmSMI@users.noreply.github.com>
2022-09-20 23:16:13 -0700

    Delete Get-AutopilotHardwareID.ps1

--- Get-AutopilotHardwareID.ps1
+++
@@ -1,5 +0,0 @@
-New-Item -Type Directory -Path "C:\HWID"
-Set-Location -Path "C:\HWID"
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
-Install-Script -Name Get-WindowsAutoPilotInfo
-Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
